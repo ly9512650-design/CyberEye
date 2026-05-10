a623df3e0e9e6fda2d7ee10c4ccdf4eac1d4a595
ly9512650-design <ly9512650@gmail.com>
2026-05-10 23:46:48 +0300

    Add banner display for CyberEye toolkit

---
+++ main.py
@@ -0,0 +1,8 @@
+from rich import print
+import pyfiglet
+from colorama import Fore
+
+banner = pyfiglet.figlet_format("CyberEye")
+
+print(f"[green]{banner}[/green]")
+print(Fore.CYAN + "Advanced Cybersecurity Toolkit")
